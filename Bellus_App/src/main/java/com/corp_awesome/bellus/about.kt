30732cbb852b80014c30b4be7b25d066d2969009
Javi Gomez <rtomyj@gmail.com>
2018-01-04 23:11:53 -0600

    added picture for the 'about me' activity, added func to about.kt and changed look of about_activity.xml

--- Bellus_App/src/main/java/com/corp_awesome/bellus/about.kt
+++ Bellus_App/src/main/java/com/corp_awesome/bellus/about.kt
@@ -1,7 +1,12 @@
 package com.corp_awesome.bellus
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 /**
  *  Activity class that displays info about the services/service provider
@@ -16,6 +21,29 @@ class about : AppCompatActivity(){
 
         supportActionBar?.title = resources.getString(R.string.about)
 
+        val phoneTV  : TextView= findViewById(R.id.phone_TV)
+        val emailTV : TextView = findViewById(R.id.email_TV)
+
+        phoneTV.isLongClickable = true
+        emailTV.isLongClickable = true
 
     }
+
+    fun smsClick(v : View){
+        val number = resources.getString(R.string.primary_phone_number)
+        val sms = Intent(Intent.ACTION_SENDTO, Uri.fromParts("sms", number , null))
+
+        startActivity(Intent.createChooser(sms, "Inquire email..."))
+
+
+    }
+
+    fun emailClick(v : View){
+        val address = resources.getString(R.string.primary_email_address)
+        val email = Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", address , null))
+
+        startActivity(Intent.createChooser(email, "Inquire email..."))
+
+    }
+
 }

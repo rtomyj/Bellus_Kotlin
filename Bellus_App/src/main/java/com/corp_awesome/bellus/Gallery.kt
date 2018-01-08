1db5661aa287be8b030578568b36558dea51709e
Javi Gomez <rtomyj@gmail.com>
2018-01-07 23:25:14 -0600

    new animation for picture preview popup, tweaked layout for the preview view

--- Bellus_App/src/main/java/com/corp_awesome/bellus/Gallery.kt
+++ Bellus_App/src/main/java/com/corp_awesome/bellus/Gallery.kt
@@ -1,15 +1,19 @@
 package com.corp_awesome.bellus
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.design.widget.CoordinatorLayout
 import android.support.v7.app.AppCompatActivity
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import java.io.File
+import android.view.animation.AnimationUtils
+import android.view.animation.Animation
+
+
+
 
 /**
  *
@@ -35,16 +39,35 @@ class Gallery : AppCompatActivity() {
             init = true
         }
 
+        var file = File(filesDir, "")
+        var list = file.listFiles()
+
+        for (item : File in list)
+            System.out.println(item.name)
+           // System.out.println(file.absolutePath + " " + file.exists())
+
     }
 
     fun showPreview(v : View){
         val previewParent : CoordinatorLayout= LayoutInflater.from(this).inflate(R.layout.gallery_preview, null) as CoordinatorLayout
         val img  = previewParent.findViewById<ImageView>(R.id.preview_iv)
+        val fab  = previewParent.findViewById<ImageView>(R.id.fab)
 
-        Glide.with(this).load(R.drawable.message_icon).into(img)
+        previewParent.visibility =View.INVISIBLE
+
+        Glide.with(this).load(R.drawable.gallery_p_4).into(img)
 
         frame.addView(previewParent)
         frame.getChildAt(0).isEnabled = false
+
+
+        previewParent.visibility =View.VISIBLE
+
+        val animation1 = AnimationUtils.loadAnimation(applicationContext,
+                R.anim.pop_up)
+        img.startAnimation(animation1)
+        fab.startAnimation(animation1)
+
     }
 
     fun exitPreview(v : View){
